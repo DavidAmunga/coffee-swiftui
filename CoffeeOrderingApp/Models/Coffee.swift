63098fa998781f3bfe6b6e77b5d8917f277dd9eb
David Amunga <david.onyimbo@gmail.com>
2020-02-13 10:06:49 +0300

    Inital Commit

---
+++ CoffeeOrderingApp/Models/Coffee.swift
@@ -0,0 +1,29 @@
+//
+//  Coffee.swift
+//  CoffeeOrderingApp
+//
+//  Created by David Amunga on 13/02/2020.
+//  Copyright © 2020 David Amunga. All rights reserved.
+//
+
+import Foundation
+
+struct Coffee{
+    let name: String
+    let imageURL : String
+    let price: Double
+}
+
+
+extension Coffee{
+    static func all()->[Coffee]{
+        return[
+            Coffee(name: "Capuccino", imageURL: "Capuccino", price: 2.5),
+            
+            Coffee(name: "Espresso", imageURL: "Espresso", price: 2.1)
+            
+            Coffee(name: "Regular", imageURL: "Regular", price: 3.0)
+        ]
+    }
+}
+
---
+++ CoffeeOrderingApp/Models/Order.swift
@@ -0,0 +1,14 @@
+//
+//  Order.swift
+//  CoffeeOrderingApp
+//
+//  Created by David Amunga on 13/02/2020.
+//  Copyright © 2020 David Amunga. All rights reserved.
+//
+
+import Foundation
+
+
+struct Order{
+    
+}
