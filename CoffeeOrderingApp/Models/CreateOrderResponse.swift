9aaa8ef394da93218132235f747cf1cbffb57667
David Amunga <david.onyimbo@gmail.com>
2020-02-13 10:51:30 +0300

    Create Order WebService

---
+++ CoffeeOrderingApp/Models/CreateOrderResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CreateOrderResponse.swift
+//  CoffeeOrderingApp
+//
+//  Created by David Amunga on 13/02/2020.
+//  Copyright © 2020 David Amunga. All rights reserved.
+//
+
+import Foundation
+
+struct CreateOrderResponse:Decodable{
+    let success: Bool
+}
